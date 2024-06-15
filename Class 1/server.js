6e1b5e7969ed021ce1a9e6ea78ecf6811adb3cb8
[OwaisWebDeveloper] <[owais.warsi0311@gmail.com]>
2024-06-15 07:54:23 -0700

    Introduction And Configuration Of Backend

---
+++ Class 1/server.js
@@ -0,0 +1,53 @@
+//--------------------------------------- START Class 1 Tuesday 14-May-2024 ------------------------------------- 
+
+// console.log("Welcome to Node JS!");
+// console.log("BACK-END DEVELOPMENT !'");
+
+// const a = 1;
+// const b = 4;
+// const c = a+b;
+// console.log(c) 
+
+// const firstName = "John";
+// const lastName = "Smith";
+// const fullName = firstName + lastName;
+// console.log("Full Name", fullName);
+
+// const fullName = `${firstName} ${lastName}`
+// console.log(`Full Name: ${fullName}`);
+
+// for (let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// function greeting () {
+//     console.log("Hello World!")
+// }
+
+// const greeting = () => console.log("Hello World!") 
+
+// greeting()
+// console.log(`Type: ${typeof greeting}`)
+
+// const employees = [
+//     {
+//         id : "1",
+//         employeeName : "Owais"
+//     },
+//     {
+//         id : "2",
+//         employeeName : "Zoha"
+//     },
+//     {
+//         id : "3",
+//         employeeName : "Obaid"
+//     },
+//     {
+//         id : "4",
+//         employeeName : "Zainab"
+//     },
+// ]
+
+// console.log(employees)
+
+// --------------------------------------------------- Class End -------------------------------------------------
